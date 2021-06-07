faa51006d2517dd21f0a46c1dc17e7bbf6958450
Sunny Golovine <sunny@glvn.co>
2021-06-07 13:37:42 -0400

    Update babel, typescript and other configs

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
+export const App: React.FC = () => {
   return (
     <SafeAreaView>
       <View style={styles.container}>
@@ -23,5 +23,3 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
-
-export default App;
